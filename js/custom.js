de5ebed6ba39bb7cc26260a840c58d1d1adb9d49
Mostafij Foysal <mostafijurrahmanfoysal635.com>
2024-04-15 23:29:47 +0600

    demo

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+$('.team-slick').slick({
+    // infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:".next",
+    prevArrow:".prev"
+  });
+
+
+
+  //lightbox//
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  })
+              
